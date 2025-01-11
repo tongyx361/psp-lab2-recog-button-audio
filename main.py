025212b548a28179751bbe8d96d8235b4fa693d2
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-01-12 01:20:21 +0800

    chore: format with ruff

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import librosa
-import numpy as np
 import argparse
 
+import librosa
+import numpy as np
+
+
 def key_tone_recognition(audio_array):
-    '''
-        请大家实现这一部分代码
-    '''
+    """
+    请大家实现这一部分代码
+    """
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--audio_file', type = str, help = 'test file name', required = True)
+    parser.add_argument("--audio_file", type=str, help="test file name", required=True)
     args = parser.parse_args()
-    input_audio_array = librosa.load(args.audio_file, sr = 48000, dtype = np.float32) # audio file is numpy float array
+    input_audio_array = librosa.load(
+        args.audio_file, sr=48000, dtype=np.float32
+    )  # audio file is numpy float array
     key_tone_recognition(input_audio_array)
